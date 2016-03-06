c630ae7a36b0218d9176d7112f6e49c61d8f77af
YilinGUO <guoyilin@hotmail.com>
2016-03-06 15:31:56 -0500

    deleted all

--- createUsers.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE screening;
-USE screening;
-
-CREATE TABLE UserName
-(
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  user_name VARCHAR(40) NOT NULL,
-  pass VARCHAR(40) NOT NULL
-);
--- insertUsers.sql
+++
@@ -1,2 +0,0 @@
-USE screening;
-INSERT INTO  UserName (user_name, pass) VALUES  ("guoyilin","123456");
