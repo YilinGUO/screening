1469f49694433723c806080795e4ca97382372a8
YilinGUO <guoyilin@hotmail.com>
2016-03-07 13:16:11 -0500

    modified:   .gitignore 	modified:   .idea/workspace.xml 	modified:   index.php

--- index.php
+++ index.php
@@ -6,12 +6,21 @@
 <?php
 include('connect-db.php');
 
-$result = $connection->query("SELECT pname, pdescp, A.url, first_name, middle_name, last_name, pcontent FROM project A, people B, people_project C WHERE C.people_id = B.id AND C.project_id = A.id");
+$result = $connection->query("SELECT id, pname, pdescp, url, pcontent FROM project");
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["pname"] . " " . $row["pdescp"]. " " . $row["url"]. "<br>";
+        $id = $row["id"];
+        echo $row["pname"]  . "<br>";
+        $people = $connection->query("SELECT first_name, middle_name, last_name FROM people, people_project WHERE '$id' = people_project.project_id AND people_project.people_id = people.id");
+        while($prow = $people->fetch_assoc()) {
+            echo $prow["first_name"] . " " . $prow["middle_name"] . " " . $prow["last_name"] . " ";
+        }
+        echo "<br>";
+        echo $row["pdescp"]. "<br>";
+        echo $row["url"]. "<br>";
+        echo $row["pcontent"]. "<br>";
     }
 } else {
     echo "0 results";
